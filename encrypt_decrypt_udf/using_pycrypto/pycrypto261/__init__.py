c67bee85570a44e21d9932d50886b4ab8306f42b
Kawish Siddiqui <kawish_siddiqui@hotmail.com>
2019-09-19 14:50:58 -0500

    Update __init__.py

---
+++ encrypt_decrypt_udf/using_pycrypto/pycrypto261/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+#
+
+"""Python Cryptography Toolkit
+
+A collection of cryptographic modules implementing various algorithms
+and protocols.
+
+Subpackages:
+
+Crypto
+"""
+
+__all__ = ['Crypto']
+__version__ = '2.6.1'     # See also below and setup.py
+__revision__ = "$Id$"
+
+# New software should look at this instead of at __version__ above.
+version_info = (2, 6, 1, 'final', 0)    # See also above and setup.py
+
